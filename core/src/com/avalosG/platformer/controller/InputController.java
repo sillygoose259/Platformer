d080f089b0c9785cb1161386bc29cb6fa52913b0
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2015-01-28 14:02:20 -0800

    updated our code and our tiled map to have nice and neat lines to make certain blocks solid. also added an array that allows us to hold our input controls.

--- core/src/com/avalosG/platformer/controller/InputController.java
+++ core/src/com/avalosG/platformer/controller/InputController.java
@@ -3,18 +3,27 @@ package com.avalosG.platformer.controller;
 import com.avalosG.platformer.model.InputControl;
 import com.avalosG.platformer.model.Spritesheet;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
+
 public class InputController {
+    private static ArrayList<InputControl> inputControls;
     private static Spritesheet spriteSheet;
     private static InputControl left;
+    private static InputControl right;
 
 
     public static void initializeController() {
+        inputControls = new ArrayList<InputControl>();
         spriteSheet = new Spritesheet("img/touch-controls.png", 80, 80);
         left = new InputControl(new Vector2(0, 0), spriteSheet.spriteFrames[0], "left");
+        right = new InputControl(new Vector2(2, 0), spriteSheet.spriteFrames[1], "right");
+        inputControls.add(left); // creating an array where we can store our input controls
+        inputControls.add(right);
         Gdx.input.setInputProcessor(createInputAdapter());
 
     }
@@ -22,7 +31,9 @@ public class InputController {
 
     public static void draw(Batch spriteBatch) {
         spriteBatch.begin();
-        left.draw(spriteBatch);
+        for(InputControl inputControl : inputControls) {
+            inputControl.draw(spriteBatch);
+        }
         spriteBatch.end();
     }
 
@@ -30,25 +41,52 @@ public class InputController {
         return new InputAdapter() {
             @Override
             public boolean keyDown(int keycode) {
-                return true;
-            }
+                if(keycode == Input.Keys.RIGHT) {
+                    PlayerController.movementAction = "right";
+                }
 
-            @Override
-            public boolean keyUp(int keycode) {
-                return true;
-            }
-
-            @Override
-            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-                if(left.getBoundingBox().contains(screenX, Gdx.graphics.getHeight() - screenY)) {
+                else if(keycode == Input.Keys.LEFT) {
                     PlayerController.movementAction = "left";
                 }
                 return true;
             }
 
             @Override
-            public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+            public boolean keyUp(int keycode) {
+                if(keycode == Input.Keys.RIGHT) {
+                    PlayerController.movementAction = "";
+                }
+                else if(keycode == Input.Keys.LEFT) {
+                    PlayerController.movementAction = "";
+                    }
+                
+                return true;
+            }
 
+            @Override
+            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+                screenY = Gdx.graphics.getHeight() - screenY;
+                for(InputControl input : inputControls) {
+                    if (input.getBoundingBox().contains(screenX, Gdx.graphics.getHeight() - screenY)) {
+                        if(input.action.equalsIgnoreCase("left")) {
+                            PlayerController.movementAction = "left";
+                        }
+                    }
+                }
+                return true;
+            }
+
+
+            @Override
+            public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+                screenY = Gdx.graphics.getHeight() - screenY;
+                for(InputControl input : inputControls) {
+                    if (input.getBoundingBox().contains(screenX, Gdx.graphics.getHeight() - screenY)) {
+                        if(input.action.equalsIgnoreCase("right")) {
+                            PlayerController.movementAction = "";
+                        }
+                    }
+                }
                 return true;
             }
         };
